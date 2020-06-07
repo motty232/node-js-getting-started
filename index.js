6cf7dccaab605e2114463574978843162c53002e
Motty <motty232@gmail.com>
2020-06-07 14:39:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
     'x-trustifi-secret': process.env.TRUSTIFI_SECRET
   },
    json: {
-  "title": "Welcome to my App",
+  "title": "Welcome to my new App",
   "html": "Hi Motty,Welcome to my new app",
   "recipients": [{"email": "mmizrahitest@outlook.com", "name": "test", "phone":{"country_code":"+1","phone_number":"1111111111"}}],
   "lists": [],
